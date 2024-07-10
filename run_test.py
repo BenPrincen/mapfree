0ebae6819788b1f906132ec37268d014e7ccc0cd
Benjamin Princen <princen.benjamin@gmail.com>
2024-07-10 14:15:16 -0700

    fixed run_test.py to use correct tests path.

--- run_test.py
+++ run_test.py
@@ -2,7 +2,7 @@ import unittest
 
 # Discover all tests in the 'tests' directory matching the pattern 'test_*.py'
 loader = unittest.TestLoader()
-suite = loader.discover("tests", pattern="test_*.py")
+suite = loader.discover("lib/tests", pattern="test_*.py")
 
 # Run the test suite
 runner = unittest.TextTestRunner()
