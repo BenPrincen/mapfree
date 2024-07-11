d6ee6cc216e33a1a8ef4525dd725c8dbac9684be
John Princen <jprincen@gmail.com>
2024-07-11 06:33:50 -0700

    Add two more points to test

--- lib/tests/test_unproject_points.py
+++ lib/tests/test_unproject_points.py
@@ -9,12 +9,12 @@ class TestUnprojectPoints(unittest.TestCase):
         self._camera = Camera(590, 590, 270, 360, 540, 720)
 
     def test_uniform_depth(self):
-        pts_y = np.array([360, 0, 719])
-        pts_x = np.array([240, 0, 539])
+        pts_y = np.array([360, 0, 719, 0, 719])
+        pts_x = np.array([240, 0, 539, 539, 0])
         pts = np.stack([pts_x, pts_y], axis=1).squeeze()
         depth_image = np.int16(np.ones((720, 540)) * 3000)
         pts_3d = unproject_points(pts, depth_image, self._camera)
-        self.assertEqual(pts_3d.shape, (3, 3))
+        self.assertEqual(pts_3d.shape, (5, 3))
         # Project back to get points - should be equal
         for pt3, pt2 in zip(pts_3d, pts):
             proj_pt = self._camera.project(pt3)
